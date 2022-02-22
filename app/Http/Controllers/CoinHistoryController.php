b798bb196a33fe2ee5153de795cf24444289d3ce
Aio <AioDev.JS@gmail.com>
2022-02-22 14:33:44 +0900

    ADD - Coin History Api

--- app/Http/Controllers/CoinHistoryController.php
+++ app/Http/Controllers/CoinHistoryController.php
@@ -4,7 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCoinHistoryRequest;
 use App\Http\Requests\UpdateCoinHistoryRequest;
+use App\Http\Resources\CoinHistoryCollection;
+use App\Http\Resources\CoinHistoryResource;
 use App\Models\CoinHistory;
+use App\Models\User;
+use App\Models\UserTicket;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use function App\Helper\applyDefaultFSW;
 
 class CoinHistoryController extends Controller
 {
@@ -13,59 +21,59 @@ class CoinHistoryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $query = CoinHistory::query();
+        $query = applyDefaultFSW($request, $query);
+
+        return new CoinHistoryCollection($query->paginate($request->get('per_page') ?: 40));
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StoreCoinHistoryRequest  $request
+     * @param \App\Http\Requests\StoreCoinHistoryRequest $request
      * @return \Illuminate\Http\Response
      */
     public function store(StoreCoinHistoryRequest $request)
     {
-        //
+        $input = $request->all();
+
+        DB::beginTransaction();
+
+        try {
+
+            User::query()->where('id', Auth::id())->decrement('coin', $request->variation);
+            $coinHistory = CoinHistory::create($input);
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw  $e;
+        }
+
+        return (new CoinHistoryResource($coinHistory))->response();
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\CoinHistory  $coinHistory
+     * @param \App\Models\CoinHistory $coinHistory
      * @return \Illuminate\Http\Response
      */
     public function show(CoinHistory $coinHistory)
     {
-        //
+        return (new CoinHistoryResource($coinHistory))->response();
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\CoinHistory  $coinHistory
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(CoinHistory $coinHistory)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdateCoinHistoryRequest  $request
-     * @param  \App\Models\CoinHistory  $coinHistory
+     * @param \App\Http\Requests\UpdateCoinHistoryRequest $request
+     * @param \App\Models\CoinHistory $coinHistory
      * @return \Illuminate\Http\Response
      */
     public function update(UpdateCoinHistoryRequest $request, CoinHistory $coinHistory)
@@ -76,11 +84,13 @@ class CoinHistoryController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\CoinHistory  $coinHistory
+     * @param \App\Models\CoinHistory $coinHistory
      * @return \Illuminate\Http\Response
      */
     public function destroy(CoinHistory $coinHistory)
     {
-        //
+        $coinHistory->deleteOrFail();
+
+        return response(["msg" => 'sucess to delete chat, id:' . $coinHistory->id]);
     }
 }
