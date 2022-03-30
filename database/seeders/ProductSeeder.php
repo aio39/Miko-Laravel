1213e3cf83619b815a4e51f034e0d8b6678e9de9
sms217 <minsung0316@naver.com>
2022-03-30 19:56:11 +0900

    EDIT - imageURL

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -23,7 +23,7 @@ class ProductSeeder extends Seeder
                 'price' => '4600',
                 'name' => "トリプルイヤー記念メタリックTシャツ",
                 'detail' => "2020年 天龍源一郎古希 天龍プロジェクト10周年 引退5周年とおめでたいことが重なったトリプルイヤーを記念して超豪華なメタリック(金箔押し)Tシャツは フロントのRevolutionロゴ、バックの記念日である2月2日(天龍源一郎誕生日)4月19日(天龍プロジェクト旗揚げ日)11月15日(引退記念日)の数字、すべてが金色に輝くデザインをなっております！ 絶対にゲットして頂きたい記念グッズです！",
-                'image' => "https://www.tenryuproject.jp/data/tenryu0419/product/20201114_6645ce.jpg"
+                'image' => "ten.jpeg"
             ],
             [
                 'created_at' => now(),
@@ -32,7 +32,7 @@ class ProductSeeder extends Seeder
                 'price' => '4600',
                 'name' => "トリプルイヤー記念メタリックTシャツ",
                 'detail' => "2020年 天龍源一郎古希 天龍プロジェクト10周年 引退5周年とおめでたいことが重なったトリプルイヤーを記念して超豪華なメタリック(金箔押し)Tシャツは フロントのRevolutionロゴ、バックの記念日である2月2日(天龍源一郎誕生日)4月19日(天龍プロジェクト旗揚げ日)11月15日(引退記念日)の数字、すべてが金色に輝くデザインをなっております！ 絶対にゲットして頂きたい記念グッズです！",
-                'image' => "https://www.tenryuproject.jp/data/tenryu0419/product/20201114_6645ce.jpg"
+                'image' => "ten.jpeg"
             ],[
                 'created_at' => now(),
                 'updated_at' => now(),
@@ -40,7 +40,7 @@ class ProductSeeder extends Seeder
                 'price' => '4600',
                 'name' => "トリプルイヤー記念メタリックTシャツ",
                 'detail' => "2020年 天龍源一郎古希 天龍プロジェクト10周年 引退5周年とおめでたいことが重なったトリプルイヤーを記念して超豪華なメタリック(金箔押し)Tシャツは フロントのRevolutionロゴ、バックの記念日である2月2日(天龍源一郎誕生日)4月19日(天龍プロジェクト旗揚げ日)11月15日(引退記念日)の数字、すべてが金色に輝くデザインをなっております！ 絶対にゲットして頂きたい記念グッズです！",
-                'image' => "https://www.tenryuproject.jp/data/tenryu0419/product/20201114_6645ce.jpg"
+                'image' => "ten.jpeg"
             ],[
                 'created_at' => now(),
                 'updated_at' => now(),
@@ -48,7 +48,7 @@ class ProductSeeder extends Seeder
                 'price' => '4600',
                 'name' => "トリプルイヤー記念メタリックTシャツ",
                 'detail' => "2020年 天龍源一郎古希 天龍プロジェクト10周年 引退5周年とおめでたいことが重なったトリプルイヤーを記念して超豪華なメタリック(金箔押し)Tシャツは フロントのRevolutionロゴ、バックの記念日である2月2日(天龍源一郎誕生日)4月19日(天龍プロジェクト旗揚げ日)11月15日(引退記念日)の数字、すべてが金色に輝くデザインをなっております！ 絶対にゲットして頂きたい記念グッズです！",
-                'image' => "https://www.tenryuproject.jp/data/tenryu0419/product/20201114_6645ce.jpg"
+                'image' => "ten.jpeg"
             ],[
                 'created_at' => now(),
                 'updated_at' => now(),
@@ -56,7 +56,15 @@ class ProductSeeder extends Seeder
                 'price' => '4600',
                 'name' => "トリプルイヤー記念メタリックTシャツ",
                 'detail' => "2020年 天龍源一郎古希 天龍プロジェクト10周年 引退5周年とおめでたいことが重なったトリプルイヤーを記念して超豪華なメタリック(金箔押し)Tシャツは フロントのRevolutionロゴ、バックの記念日である2月2日(天龍源一郎誕生日)4月19日(天龍プロジェクト旗揚げ日)11月15日(引退記念日)の数字、すべてが金色に輝くデザインをなっております！ 絶対にゲットして頂きたい記念グッズです！",
-                'image' => "https://www.tenryuproject.jp/data/tenryu0419/product/20201114_6645ce.jpg"
+                'image' => "ten.jpeg"
+            ],[
+                'created_at' => now(),
+                'updated_at' => now(),
+                'concert_id' => 1,
+                'price' => '4600',
+                'name' => "トリプルイヤー記念メタリックTシャツ",
+                'detail' => "2020年 天龍源一郎古希 天龍プロジェクト10周年 引退5周年とおめでたいことが重なったトリプルイヤーを記念して超豪華なメタリック(金箔押し)Tシャツは フロントのRevolutionロゴ、バックの記念日である2月2日(天龍源一郎誕生日)4月19日(天龍プロジェクト旗揚げ日)11月15日(引退記念日)の数字、すべてが金色に輝くデザインをなっております！ 絶対にゲットして頂きたい記念グッズです！",
+                'image' => "ten.jpeg"
             ],[
                 'created_at' => now(),
                 'updated_at' => now(),
@@ -64,7 +72,7 @@ class ProductSeeder extends Seeder
                 'price' => '4460',
                 'name' => "ミュージカル レベッカ OST リパッケージ (Das Musical Rebecca Repackage Album 2017)",
                 'detail' => "2013年の実況録音(3CD)と2014年·2016年·2017年を代表するナンバー「一日また一日」、「神よ」、「レベッカ(長いバージョン)」、「永遠の命」などを含むSpecial CDを含むミュージカル「レベッカO.S.T.」のリパッケージアルバムが9月25日(月)に発売される。",
-                'image' => "http://image.yes24.com/momo/TopCate1508/MidCate010/150793233.png"
+                'image' => "rebecaProduct.png"
             ],[
                 'created_at' => now(),
                 'updated_at' => now(),
@@ -72,7 +80,7 @@ class ProductSeeder extends Seeder
                 'price' => '1980',
                 'name' => "Leap of faith 【初回限定盤】",
                 'detail' => "2009年3月にてボーカルnaoが脱退。2009年10月、南條愛乃をボーカルに迎え、TVアニメ「とある科学の超電磁砲（レールガン）」のOPテーマにて再始動が決定！",
-                'image' => "https://img.hmv.co.jp/image/jacket/190/0000124/5/0/919.jpg"
+                'image' => "fripSide.jpeg"
             ]
         ]);
     }
