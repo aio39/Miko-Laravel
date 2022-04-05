e378fe1c6506ee21adb6dc182328c292974fc21e
Aio <AioDev.JS@gmail.com>
2022-04-05 10:08:11 +0900

    UPDATE - FSW
    filter의 값이 -1일 경우 무시됨.
    sort의 기본은 id desc 적용

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -1,13 +1,16 @@
 <?php
+
 namespace App\Helper;
+
 use Carbon\Carbon;
 use Illuminate\Support\Str;
 
-if (! function_exists('applyDefaultFindById')) {
-    function applyDefaultFindById($request, $query){
+if (!function_exists('applyDefaultFindById')) {
+    function applyDefaultFindById($request, $query)
+    {
         $query->when(request()->filled('with'), function ($query) {
-            $withs = explode(',',request('with'));
-            if($withs){
+            $withs = explode(',', request('with'));
+            if ($withs) {
                 $query->with($withs);
             }
             return $query;
@@ -19,42 +22,46 @@ if (! function_exists('applyDefaultFindById')) {
 }
 
 
-if (! function_exists('applyDefaultFSW')) {
+if (!function_exists('applyDefaultFSW')) {
     function applyDefaultFSW($request, $query)
     {
         ////        ?filter=category:food,id:2
         $query->when(request()->filled('filter'), function ($query) {
-            $filters = explode(',',request('filter'));
-            foreach ($filters as $filter){
-                [$criteria,$value] = explode(':',$filter);
-                $query->where($criteria,$value);
+            $filters = explode(',', request('filter'));
+            foreach ($filters as $filter) {
+                [$criteria, $value] = explode(':', $filter);
+                if ($value !== "-1") {
+                    $query->where($criteria, $value);
+                }
             }
             return $query;
         });
 
 
 //        ?sort=-id,price
-        if($request->sort){
-            $sorts =  explode(',' ,$request->input('sort',''));
-            foreach ($sorts as $sortColumn){
-                $sortDirection = Str::startsWith($sortColumn,'-') ? 'desc':'asc';
-                $sortColumn = ltrim($sortColumn,'-');
-                $query->orderBy($sortColumn,$sortDirection);
+        if ($request->sort) {
+            $sorts = explode(',', $request->input('sort', ''));
+            foreach ($sorts as $sortColumn) {
+                $sortDirection = Str::startsWith($sortColumn, '-') ? 'desc' : 'asc';
+                $sortColumn = ltrim($sortColumn, '-');
+                $query->orderBy($sortColumn, $sortDirection);
             }
+        } else {
+            $query->orderBy('id','desc');
         }
 
 
         // ?with=shop,menu
         $query->when(request()->filled('with'), function ($query) {
-            $withs = explode(',',request('with'));
-            if($withs){
+            $withs = explode(',', request('with'));
+            if ($withs) {
                 $query->with($withs);
             }
             return $query;
         });
 
         $query->when(request()->filled('start') && request()->filled('end'), function ($query) {
-            $query->whereBetween('created_at',[request('start'),request('end')]);
+            $query->whereBetween('created_at', [request('start'), request('end')]);
             return $query;
 
 //            $start = Carbon::createFromTimestamp(request('start'))->toDateTimeString();
@@ -64,15 +71,13 @@ if (! function_exists('applyDefaultFSW')) {
         });
 
         $query->when(request()->filled('up-start') && request()->filled('up-end'), function ($query) {
-            $query->whereBetween('created_at',[request('up-start'),request('up-end')]);
+            $query->whereBetween('created_at', [request('up-start'), request('up-end')]);
             return $query;
         });
 
 
-
         return $query;
     }
-
 
 
 }
